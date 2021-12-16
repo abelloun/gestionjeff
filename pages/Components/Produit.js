2c547fdc292a00f95fc73e1507fdc6e76d37f6a4
you@example.com <you@example.com>
2021-12-16 16:48:06 +0100

    Text(Area)Edit

--- pages/Components/Produit.js
+++ pages/Components/Produit.js
@@ -1,16 +1,26 @@
 import NumberEdit from './NumberEdit.js';
+import TextEdit from './TextEdit.js';
+import TextAreaEdit from './TextAreaEdit.js';
 
 export default function Produit({produit: p, setProduit}) {
 	
 	function setQuantity(q) {
 		setProduit({...p, quantity: q});
 	}
+	
+	function setName(n) {
+		setProduit({...p, name: n});
+	}
+	
+	function setDescription(d) {
+		setProduit({...p, description: d});
+	}
 
 	return (
 		<tr>
-			<td>{p.name}</td>
+			<td><TextEdit value={p.name} setValue={setName} /></td>
 			<td><NumberEdit value={p.quantity} setValue={setQuantity} /></td>
-			<td>{p.description}</td>
+			<td><TextAreaEdit value={p.description} setValue={setDescription} /></td>
 		</tr>
 	);
 }
